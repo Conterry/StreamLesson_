285f6d15386d5bfbb66fa403f74473d976cada83
Hleb Kriukov <glebos05@gmail.com>
2020-12-17 19:18:55 +0200

    Lesson Task Compeated

---
+++ StreamLesson/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace StreamLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string FileWay = "Input.txt";
+            string OutputFileWay = "Output.txt";
+
+            string InputText = ReadFile(FileWay);
+            WriteFile(OutputFileWay, InputText);
+            if(ReadFile(OutputFileWay) == null)
+            {
+                Console.WriteLine("I think everything is much worse");
+            }
+        }
+
+        public static string ReadFile(string Way)
+        {
+            StreamReader sr = new StreamReader(Way);
+            string InputText = sr.ReadLine();
+            return InputText;
+        }
+
+        public static void WriteFile(string Way, string Text)
+        {
+            StreamWriter sw = new StreamWriter(Way);
+            sw.WriteLine(Text);
+            sw.Close();
+        }
+    }
+}
